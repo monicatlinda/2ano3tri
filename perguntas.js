469e2b9636718e5d0d756354728b1e39a0a8565b
monicatlinda <163007510+monicatlinda@users.noreply.github.com>
2024-09-16 15:54:39 -0300

    fa

--- perguntas.js
+++ perguntas.js
@@ -11,13 +11,13 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Português',
+    'O que significa a palavra estotérica?',
+    'algo que é entendido apenas por poucas pessoas.'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz praia em Inglês?',
+    'Oi em ingles é beach'
 )
